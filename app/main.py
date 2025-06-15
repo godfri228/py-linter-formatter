a4415934da17280669e34fd727dc2698644ed94c
godfri228 <overe14@gmail.com>
2025-06-15 15:12:48 +0300

    Implement linter formatter functions with all tests passed

--- app/main.py
+++ app/main.py
@@ -1,13 +1,43 @@
-def format_linter_error(error: dict) -> dict:
-    # write your code here
-    pass
+def format_linter_error(error):
+    return {
+        "line": error["line_number"],
+        "column": error["column_number"],
+        "message": error["text"],
+        "name": error["code"],
+        "source": "flake8"
+    }
 
+def format_single_linter_file(path, errors):
+    return {
+        "path": path,
+        "status": "failed" if errors else "passed",
+        "errors": [
+            {
+                "line": e["line_number"],
+                "column": e["column_number"],
+                "message": e["text"],
+                "name": e["code"],
+                "source": "flake8"
+            }
+            for e in errors
+        ]
+    }
 
-def format_single_linter_file(file_path: str, errors: list) -> dict:
-    # write your code here
-    pass
-
-
-def format_linter_report(linter_report: dict) -> list:
-    # write your code here
-    pass
+def format_linter_report(errors):
+    return [
+        {
+            "path": path,
+            "status": "failed" if file_errors else "passed",
+            "errors": [
+                {
+                    "line": e["line_number"],
+                    "column": e["column_number"],
+                    "message": e["text"],
+                    "name": e["code"],
+                    "source": "flake8"
+                }
+                for e in file_errors
+            ]
+        }
+        for path, file_errors in errors.items()
+    ]
